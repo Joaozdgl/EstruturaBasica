4a3b10d562ce8ae2129e23d81be1aa8f482b4c71
Joaozdgl <joaomanoelassis25@gmail.com>
2023-06-16 12:08:13 -0300

    segundo ponto

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("input")
+const button = document.getElementById("button")
+button.onclick =()=>{
+    let ok='' 
+    ok = input.value
+    alert("esse é o seu nome" + ok)
+}
+
+
